1b2b9a8e0e71e2c6ef2510094ee2c964fd7886a4
annathunberg <anna.thunberg1@gmail.com>
2025-03-25 14:01:10 +0100

    user functionality frontend + landingpage

--- frontend/src/components/loginPage.jsx
+++ frontend/src/components/loginPage.jsx
@@ -1,10 +1,12 @@
 // src/pages/LoginPage.js
 import { useState } from "react";
-import login from "../hooks/authApi";
+import { useNavigate, Link } from "react-router-dom";
+import AuthApi from "../hooks/AuthApi"; // Se till att denna import används
 import useUser from "../store/userStore";
 import "./loginPage.css";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -13,16 +15,18 @@ const LoginPage = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const data = await login(email, password);
-      const { user, token } = data; // Assuming you get user and token from your backend
+      const data = await AuthApi.login(email, password);
+      const { user, token } = data;
+      console.log(token.existingUser);
 
       // Store user data and token in Zustand store
-      setUser(user, token);
-
+      setUser(token.existingUser, token);
+      navigate("/home");
       // Redirect user to the dashboard or another page
       // For example: navigate('/dashboard');
     } catch (err) {
       setError("Login failed. Please try again.");
+      console.error("Login error:", err); // Lägg till logg för felsökning
     }
   };
 
@@ -51,6 +55,15 @@ const LoginPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+        </div>
+
+        <div className="linkWrapper">
+          <p>
+            Don't have an account? Register{" "}
+            <Link to="/signup" className="linkText">
+              here
+            </Link>
+          </p>
         </div>
 
         <button className="userbutton" type="submit">
